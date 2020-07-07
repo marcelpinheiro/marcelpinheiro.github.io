05399b1557bbc16123dc2b027a003d57e100a380
Marcel Borges Pinheiro <marcelpinheiro@gmail.com>
2020-07-07 13:24:34 -0400

    Delete linkedin.js

--- linkedin.js
+++
@@ -1,3 +0,0 @@
-<script src="https://gist.github.com/jonschlinkert/5854601.js"></script>
-<script type="text/javascript" src="ht<span>tps://platform.linkedin.com/badges/js/profile.js" async defer></script>
-<div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="vertical" data-theme="light" data-vanity="marcelpinheiro"><a class="LI-simple-link" href='ht<span>tps://br.linkedin.com/in/marcelpinheiro?trk=profile-badge'>Marcel Pinheiro</a></div>
